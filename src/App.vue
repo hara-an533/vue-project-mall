cd906da286d02d81d4efc05d9604898b76ec7683
lengn <2497963360@qq.com>
2020-06-24 13:40:18 +0800

    “tabbar finished”

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-table-bar></main-table-bar>
   </div>
 </template>
 
 <script>
-
+import MainTableBar from "./components/common/tabbar/MainTabBar"
 
 export default {
   name: 'App',
   components: {
+    MainTableBar
   }
 }
 </script>
